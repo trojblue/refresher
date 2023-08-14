b04754ddd042398d05d59f5e6dc9a6c1c85b1dd4
yada <trojblue@gmail.com>
2023-08-13 21:24:52 -0400

    feat: updating script

--- refresh_gradient.py
+++ refresh_gradient.py
@@ -1,22 +1,33 @@
 from refresher.base import *
 from gradient import NotebooksClient
 
-API_KEY = "YOUR_API_KEY"
-PRINT_TIMEZONE = "UTC"
+import json
+
+# Read the configuration from the JSON file
+with open('config.json', 'r') as file:
+    config = json.load(file)
+
+# Extract Gradient and Lambda Labs settings
+gradient_config = config['gradient']
+lambda_labs_config = config['lambda_labs']
+
 
 def gradient_status():
-    notebooks_client = NotebooksClient(API_KEY)
+    notebooks_client = NotebooksClient(gradient_config['API_KEY'])
     response = notebooks_client.list(tags=[])
     return response[0].state
 
+
 def gradient_action(notebook_id):
-    notebooks_client = NotebooksClient(API_KEY)
+    notebooks_client = NotebooksClient(gradient_config['API_KEY'])
     notebooks_client.start(id=notebook_id, machine_type='Free-A100-80G', shutdown_timeout=6)
 
-notebooks_client = NotebooksClient(API_KEY)
-response = notebooks_client.list(tags=[])
-notebook_id = response[0].id
 
-monitor_services(service_status_func=gradient_status,
-                 service_action_func=lambda: gradient_action(notebook_id),
-                 timezone=PRINT_TIMEZONE)
+if __name__ == '__main__':
+    notebooks_client = NotebooksClient(gradient_config['API_KEY'])
+    response = notebooks_client.list(tags=[])
+    notebook_id = response[0].id
+
+    monitor_services(service_status_func=gradient_status,
+                     service_action_func=lambda: gradient_action(notebook_id),
+                     timezone=gradient_config['TIMEZONE'])
--- refresh_lambda.py
+++ refresh_lambda.py
@@ -1,11 +1,13 @@
 from refresher.base import *
 import requests
+import json
 
+# Read the configuration from the JSON file
+with open('config.json', 'r') as file:
+    config = json.load(file)
 
-API_BASE_URL = "https://cloud.lambdalabs.com/api/v1"
-KEY = "secret_nyanko-api-key"
-SSH_KEY_NAMES = ["nyanko"]
-NODE_NAME = "training-node-1"
+# Extract Gradient and Lambda Labs settings
+lambda_labs_config = config['lambda_labs']
 
 def lambda_status():
     instances = get_instance_types()["data"]
@@ -15,15 +17,15 @@ def lambda_status():
 
 def lambda_action():
     region_name = get_instance_types()["data"]["gpu_1x_h100_pcie"]["regions_with_capacity_available"][0]["name"]
-    launch_instance(region_name, "gpu_1x_h100_pcie", SSH_KEY_NAMES, 1, NODE_NAME)
+    launch_instance(region_name, "gpu_1x_h100_pcie", lambda_labs_config['SSH_KEY_NAMES'], 1, lambda_labs_config['NODE_NAME'])
 
 def get_instance_types():
-    headers = {"Authorization": f"Bearer {KEY}"}
-    response = requests.get(f"{API_BASE_URL}/instance-types", headers=headers)
+    headers = {"Authorization": f"Bearer {lambda_labs_config['KEY']}"}
+    response = requests.get(f"{lambda_labs_config['API_BASE_URL']}/instance-types", headers=headers)
     return response.json()
 
 def launch_instance(region_name, instance_type_name, ssh_key_names, quantity, name):
-    headers = {"Authorization": f"Bearer {KEY}"}
+    headers = {"Authorization": f"Bearer {lambda_labs_config['KEY']}"}
     payload = {
         "region_name": region_name,
         "instance_type_name": instance_type_name,
@@ -31,9 +33,10 @@ def launch_instance(region_name, instance_type_name, ssh_key_names, quantity, na
         "quantity": quantity,
         "name": name
     }
-    response = requests.post(f"{API_BASE_URL}/instance-operations/launch", headers=headers, json=payload)
+    response = requests.post(f"{lambda_labs_config['API_BASE_URL']}/instance-operations/launch", headers=headers, json=payload)
     return response.json()
 
-monitor_services(service_status_func=lambda_status,
-                 service_action_func=lambda_action,
-                 timezone="UTC")
+if __name__ == '__main__':
+    monitor_services(service_status_func=lambda_status,
+                     service_action_func=lambda_action,
+                     timezone="UTC")
